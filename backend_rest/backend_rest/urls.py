442f58c4c4cc4fc2c5086b0f8ebc9a671705fe52
ezrankayamba <ezrankayamba@gmail.com>
2020-03-12 00:33:24 +0300

    Context path env variable

--- backend_rest/backend_rest/urls.py
+++ backend_rest/backend_rest/urls.py
@@ -2,12 +2,19 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+import os
+
+context_path = os.environ.get('DIST_BACKEND_CONTEX_PATH')
+if context_path is None:
+    context_path = ''
+else:
+    context_path = f'{context_path}/'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('oauth2/', include('oauth2_provider.urls', namespace='oauth2_provider')),
-    path('users/', include('users.urls')),
-    path('', include('customers.urls')),
+    path(f'{context_path}admin/', admin.site.urls),
+    path(f'{context_path}oauth2/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+    path(f'{context_path}users/', include('users.urls')),
+    path(f'{context_path}', include('customers.urls')),
 ]
 
 if settings.DEBUG:
