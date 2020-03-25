941432e4ebdb6e82d7eee1eaa185855fc3c5513e
ezrankayamba <ezrankayamba@gmail.com>
2020-03-25 17:40:43 +0300

    Search submit

--- frontend_web/src/components/utils/search/SearchForm.js
+++ frontend_web/src/components/utils/search/SearchForm.js
@@ -17,37 +17,38 @@ export const SearchForm = ({searchFields, onSearch}) => {
         setData(data)
     };
     let search = (e) => {
+        e.preventDefault()
         if (onSearch) {
             onSearch(data, (res) => {})
         }
     }
     return (
         <div className="container small">
-            <div className="pl-3 row d-md-flex search border border-top-0 border-left-0 border-right-0">
+            <form onSubmit={search} className="d-block pl-3 row d-md-flex search border border-top-0 border-left-0 border-right-0">
                 <div className="col-md pl-0">
                     <div className="row">
-                        {searchFields.map(fld => {
-                            return (
-                                <div key={fld.name} className="mb-2 col-md-3 pl-0 pr-1">
-                                    <InputControl field={fld.search}
-                                                  name={fld.search.name} id={fld.search.name}
-                                                  className="form-control p-2"
-                                                  onChange={handleChange}/>
-                                </div>)
-                        })}
+                            {searchFields.map(fld => {
+                                return (
+                                    <div key={fld.name} className="mb-2 col-md-3 pl-0 pr-1">
+                                        <InputControl field={fld.search}
+                                                      name={fld.search.name} id={fld.search.name}
+                                                      className="form-control p-2"
+                                                      onChange={handleChange}/>
+                                    </div>)
+                            })}
                     </div>
                 </div>
                 <div className="mt-auto pb-2 col-md-auto p-0">
                     <div className="row mr-0 ml-0">
                         <div className="col p-0 pr-1">
-                            <button type="button" className="btn btn-outline-secondary float-right pt-2 pb-2" onClick={search}>
+                            <button className="btn btn-outline-secondary float-right pt-2 pb-2">
                                 <IconSearch/> <span
                                 className="pl-1">Search</span>
                             </button>
                         </div>
                     </div>
                 </div>
-            </div>
+            </form>
         </div>
     )
 }
